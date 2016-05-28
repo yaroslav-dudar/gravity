c6aa84fa64bc27eb6ac3d5b73084ca9059ade0aa
Yaroslav <flayingfog@gmail.com>
2016-05-28 07:49:01 +0300

    mv sfml,box2d sources to sources/

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
-g++ -c main.cpp -I /home/morda/dev/gravity/sfml-sources/SFML-2.3/include -I /home/morda/dev/gravity/box2d-sources/Box2D-master/Box2D/
+g++ -c main.cpp -I /home/morda/dev/gravity/sources/sfml-sources/SFML-2.3/include -I /home/morda/dev/gravity/sources/box2d-sources/Box2D-master/Box2D/
 if [ $? -gt 0 ];then
     echo error main.o generation error
     exit
 fi
 
 echo main.o generated ...
-g++ main.o -o sfml-app -L/home/morda/dev/gravity/sfml-sources/SFML-2.3/lib -lsfml-graphics-d -lsfml-window-d -lsfml-system-d -L/home/morda/dev/gravity/box2d-sources/Box2D-master/Box2D/Box2D -lBox2D
+g++ main.o -o sfml-app -L/home/morda/dev/gravity/sources/sfml-sources/SFML-2.3/lib -lsfml-graphics-d -lsfml-window-d -lsfml-system-d -L/home/morda/dev/gravity/sources/box2d-sources/Box2D-master/Box2D/Box2D -lBox2D
 if [ $? -gt 0 ];then
     echo main.o compilation error
     exit
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-export LD_LIBRARY_PATH=/home/morda/dev/gravity/sfml-sources/SFML-2.3/lib:/home/morda/dev/gravity/box2d-sources/Box2D-master/Box2D/Box2D
+export LD_LIBRARY_PATH=/home/morda/dev/gravity/sources/sfml-sources/SFML-2.3/lib:/home/morda/dev/gravity/sources/box2d-sources/Box2D-master/Box2D/Box2D
 ./sfml-app
